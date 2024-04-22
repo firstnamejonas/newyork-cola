a742f9e550f3b310f8c42bad9c229ae9113897df
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-22 11:34:50 +0000

    Add product model for colas

--- colas/models.py
+++ colas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Cola(models.Model):
+    product_name = models.CharField(max_length=100, null=False, blank=False)
+    product_description = models.TextField(max_length=500, null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+    product_image = CloudinaryField('image', null=False, blank=False)
+    ingredients = models.TextField(max_length=500, null=True, blank=True)
+    nutrition_facts = models.TextField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
