98ed35eb324f1111567e3538f84c4d4fb747ee21
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-22 09:51:58 +0000

    Setup local and deployed debug method

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [
     '8000-firstnamejo-newyorkcola-ksbrda2186b.ws-eu110.gitpod.io',
