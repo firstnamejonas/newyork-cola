553eaef8f73a32baf43f26cefe4cdc1153808cc2
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-22 10:40:06 +0000

    Add cloudinary for product image storage

--- main/settings.py
+++ main/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     # Required by allauth:
     'django.contrib.sites',
     'allauth',
@@ -52,6 +53,8 @@ INSTALLED_APPS = [
     # Project-Apps:
     'home',
     'colas',
+    #cloudinary
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
