d23e7bc6243a3e90bee3c739089293f983c0a49b
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-22 12:09:40 +0000

    Add all colas view to show all products

--- colas/views.py
+++ colas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Cola
 
-# Create your views here.
+
+def all_colas(request):
+    """
+    View to show all New York Cola Products
+    (It's a one product store, so it's not necessary right now
+    but for future maintainability and more projects it's better.)
+    """
+
+    colas = Cola.objects.all()
+
+    context = {
+        'colas': colas,
+    }
+
+    return render(request, 'colas/all_colas.html', context)
