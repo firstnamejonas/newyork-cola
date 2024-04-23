8884fadee17213012a2730ea38e11d8a841e6732
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-23 13:22:29 +0000

    Add urls for remove and adjust bag items

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_items, name='add_items')
+    path('add/<item_id>/', views.add_items, name='add_items'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+    path('delete/<item_id>/', views.delete_items, name='delete_items'),
 ]
