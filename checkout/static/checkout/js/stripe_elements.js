02352a1bdd0cce5a78d1b94188309f8bad562d5a
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-24 13:47:27 +0000

    Add validation errors eventlistener

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -27,3 +27,17 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <i class="bi bi-exclamation-triangle"></i>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
