e471be54e67c31bbff5de94fd318c8aa44e1fc00
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-24 15:30:56 +0000

    Fix bug of worn variable used in string

--- checkout/models.py
+++ checkout/models.py
@@ -62,4 +62,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'Product {self.cola.product_name} on order {self.order.order_number}'
+        return f'Product {self.product.product_name} on order {self.order.order_number}'
