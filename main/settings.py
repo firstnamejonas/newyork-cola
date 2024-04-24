9849b64589ab96df577edb1f7acfb482c7d4187a
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-24 13:46:20 +0000

    Setup stripe settings

--- main/settings.py
+++ main/settings.py
@@ -177,6 +177,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Stripe:
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
