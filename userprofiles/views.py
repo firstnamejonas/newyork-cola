a6bda0f7c041917f7f2c2573b1f24db64cccba14
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-26 10:05:34 +0000

    Add functionality to view userprofile info

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 def profile(request):
     """
     View to display userprofile template.
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'userprofiles/userprofile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
