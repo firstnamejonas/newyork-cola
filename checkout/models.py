4fddb0353dfa03b74a72f2a327946ecf473a8f42
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-26 08:46:03 +0000

    Update checkout model with userprofile

--- checkout/models.py
+++ checkout/models.py
@@ -6,10 +6,13 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from colas.models import Cola
+from userprofiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     country = CountryField(null=False, blank=False, blank_label='Country *')
