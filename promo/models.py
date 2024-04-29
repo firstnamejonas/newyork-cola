3c1475fefb9d209753daf417a5dfd3de05970d7a
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-29 11:12:41 +0000

    Add model for contest entries data

--- promo/models.py
+++ promo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contest(models.Model):
+    """
+    Class to safe Contest Participants Data.
+    """
+    contest_username = models.CharField(max_length=100, null=False, blank=False)
+    contest_ordernumber = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.contest_username
