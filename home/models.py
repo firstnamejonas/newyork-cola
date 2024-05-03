92b2b9c95c6e6482a885fffd4ae3d4faf6a66f7f
Jonas Hessling <firstname.jonas@icloud.com>
2024-05-03 15:05:34 +0000

    Add model to safe newsletter user data

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsletterSignup(models.Model):
+    """
+    Class to safe newsletter form data.
+    """
+
+    newsletter_first_name = models.CharField(max_length=100, null=False, blank=False)
+    newsletter_email = models.EmailField(max_length=254, null=False, blank=False)
+
+    def __str__(self):
+        return self.newsletter_first_name
