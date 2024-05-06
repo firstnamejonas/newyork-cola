2d2012f84a8fb7006f39883e748ed551ec826693
Jonas Hessling <firstname.jonas@icloud.com>
2024-05-06 14:30:43 +0000

    Optimise colas app files format to pass validation

--- colas/models.py
+++ colas/models.py
@@ -3,12 +3,40 @@ from cloudinary.models import CloudinaryField
 
 
 class Cola(models.Model):
-    product_name = models.CharField(max_length=100, null=False, blank=False)
-    product_description = models.TextField(max_length=500, null=False, blank=False)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
-    product_image = CloudinaryField('image', null=False, blank=False)
-    ingredients = models.TextField(max_length=500, null=True, blank=True)
-    nutrition_facts = models.TextField(max_length=500, null=True, blank=True)
+    """
+    Models to store product data.
+    """
+    product_name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    product_description = models.TextField(
+        max_length=500,
+        null=False,
+        blank=False
+    )
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        blank=False
+    )
+    product_image = CloudinaryField(
+        'image',
+        null=False,
+        blank=False
+    )
+    ingredients = models.TextField(
+        max_length=500,
+        null=True,
+        blank=True
+    )
+    nutrition_facts = models.TextField(
+        max_length=500,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.product_name
