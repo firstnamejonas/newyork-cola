67f728cfae0c59442e9561e53ea87ae9e0576e69
Jonas Hessling <firstname.jonas@icloud.com>
2024-05-07 11:01:05 +0000

    Add python unit testing for contact app

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Contact
+from .forms import ContactForm
 
-# Create your tests here.
+
+class ContactFormTest(TestCase):
+    """
+    Test contact app forms.
+    """
+    def test_contact_form_valid_data(self):
+        form = ContactForm(data={
+            'contact_full_name': 'John Doe',
+            'contact_email': 'test@example.com',
+            'contact_issue': 'order',
+            'contact_message': 'Test Message'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_contact_form_invalid_data(self):
+        form = ContactForm(data={})
+        self.assertFalse(form.is_valid())
+
+
+class ContactViewTest(TestCase):
+    """
+    Test contact app views.
+    """
+    def test_contact_page_view(self):
+        response = self.client.get(reverse('contact_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact/contact_page.html')
