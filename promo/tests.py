d775c2654757763227429b96e7475b48552ac8d7
Jonas Hessling <firstname.jonas@icloud.com>
2024-05-07 12:56:52 +0000

    Add python unit testing for promo app

--- promo/tests.py
+++ promo/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Contest
+from .forms import ContestForm
+from checkout.models import Order
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ContestPageTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_contest_page_get(self):
+        """
+        Test GET request for the contest page
+        """
+        response = self.client.get(reverse('contest_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'promo/contest_page.html')
+
+    def test_contest_form_valid(self):
+        """
+        Test valid contest form submission
+        """
+        order = Order.objects.create(
+            order_number='12345'
+        )
+        form_data = {
+            'contest_username': 'testuser',
+            'contest_ordernumber': '12345'
+        }
+        form = ContestForm(data=form_data)
+        self.assertTrue(form.is_valid())
